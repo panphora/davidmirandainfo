d102442c9f2884bfd729c038e23c6bc919a76843
David Miranda <david@storylog.com>
2018-06-25 11:09:19 -0400

    back to formspree

--- scripts/main.js
+++ scripts/main.js
@@ -33,8 +33,6 @@ $(".form-group input, .form-group textarea").on("blur", function (event) {
 });
 
 $(".contact-form").on("submit", function (event) {
-  event.preventDefault();
-
   var $form = $(event.currentTarget);
   var $submitButton = $form.find(".send-button");
 
@@ -42,31 +40,34 @@ $(".contact-form").on("submit", function (event) {
   var messageWithoutWhitespace = message.replace(/\s/g, "");
 
   if (messageWithoutWhitespace.length < 15) {
+    event.preventDefault();
     var notyf = new Notyf();
     notyf.alert("Please write me a longer message before sending!");
-  } else {
-    $.ajax({
-        url: $form.prop("action"), 
-        method: "POST",
-        crossDomain: true,
-        headers : {
-          'accept' : 'application/javascript',
-        },
-        data: $form.serialize(),
-        beforeSend: function () {
-          $submitButton.prop('disabled', 'disabled');
-        },
-        success: function (resp) {
-          $form.find(".contact-me-text").html("Thank you for contacting me!<br><br>٩(^ᴗ^)۶")
-          $form.find(".form-group, .button-container").hide()
-        }
-    });
   }
 
 });
 
 
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
 
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+
+var thankYouUrlParam = getUrlParameter("thankyou");
+if (thankYouUrlParam && thankYouUrlParam === "1") {
+  $(".contact-form").find(".contact-me-text").html("Thank you for contacting me!<br><br>٩(^ᴗ^)۶")
+  $(".contact-form").find(".form-group, .button-container").hide()
+}
 
 
 
